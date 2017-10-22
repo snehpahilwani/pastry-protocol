a11a7e8437d6105b6be7e9a562207414b1dbec08
Sneh Pahilwani <snehpahilwani@gmail.com>
2017-10-22 04:05:55 -0400

    Submitting command line changes

--- lib/project3.ex
+++ lib/project3.ex
@@ -1,5 +1,11 @@
 defmodule Project3 do
   def main(args) do
-    IO.puts "Hello"
+        if Enum.count(args) == 2 do
+            {numNodes,_} = Integer.parse(Enum.at(args,0))
+            {numRequests,_} = Integer.parse(Enum.at(args,1))
+        else
+            IO.puts "Invalid number of arguments. Please provide ./project3 numNodes numRequests"
+            System.halt(0)
+        end
   end
 end
